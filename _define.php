8e4df01427198b967b8079c3667aa540659732d6
franck <carnet.franck.paul@gmail.com>
2023-07-08 10:07:28 +0200

    New release 2.3.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Recently Published Posts Dashboard Module',
     'Display recently published posts on dashboard',
     'Franck Paul',
-    '2.3.1',
+    '2.3.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
