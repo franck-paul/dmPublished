81d1576073abec7f0c5ad5d02c1e1d67dddf6671
franck <carnet.franck.paul@gmail.com>
2023-07-08 10:07:06 +0200

    Fix requires

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
     'Franck Paul',
     '2.3.1',
     [
-        'requires'    => [['core', '2.3.16']],
+        'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
             dcAuth::PERMISSION_ADMIN,
         ]),
