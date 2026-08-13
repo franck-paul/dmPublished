ad727eb64963bca591711961872b6add9b7ce62e
franck <contact@open-time.net>
2026-08-13 16:40:08 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,26 +11,30 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Recently Published Posts Dashboard Module',
-    'Display recently published posts on dashboard',
-    'Franck Paul',
-    '10.0',
-    [
-        'date'     => '2026-08-03T09:53:44+0200',
-        'requires' => [
-            ['core', '2.39'],
-            ['dmHelper', '5.0'],
-        ],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [
-            'pref' => '#user-favorites.dmpublished',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=dmPublished',
-        'support'    => 'https://github.com/franck-paul/dmPublished',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/dmPublished/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Recently Published Posts Dashboard Module',
+        'Display recently published posts on dashboard',
+        'Franck Paul',
+        '10.0',
+        [
+            'date'     => '2026-08-03T09:53:44+0200',
+            'requires' => [
+                ['core', '2.39'],
+                ['dmHelper', '5.0'],
+            ],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [
+                'pref' => '#user-favorites.dmpublished',
+            ],
+
+            'details'    => 'https://open-time.net/?q=dmPublished',
+            'support'    => 'https://github.com/franck-paul/dmPublished',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/dmPublished/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
